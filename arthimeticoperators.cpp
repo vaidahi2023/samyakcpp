a4983925236ba00c21f2523ed0c3264aaab1d820
vaidahi2023 <vaidahi810@gmail.com>
2026-04-13 17:52:41 +0530

    introduction to cpp

---
+++ arthimeticoperators.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+        int a,b,sum,sub,mul,div,mod;
+        cout<<"enter the number:";
+        cin>>a>>b;
+        sum=a+b;
+        cout<<"sum="<<sum<<endl;
+        sub=a-b;
+        cout<<"sub="<<sub<<endl;
+        mul=a*b;
+        cout<<"mul="<<mul<<endl;
+        div=a/b;
+        cout<<"div="<<div<<endl;
+        
+}
---
+++ datatype.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    float f;
+    double d;
+    char c;
+    bool b;
+    cout<<"enter the number:";
+    cin>>a;
+    cout<<"a="<<a<<endl;
+     cout<<"enter the number:";
+    cin>>f;
+    cout<<"f="<<f<<endl;
+    cout<<"enter the number:";
+    cin>>d;
+    cout<<"d="<<d<<endl;
+    cout<<"enter the chracter:";
+    cin>>c;
+    cout<<"c="<<c<<endl;
+    cout<<"enter the boolean:";
+    cin>>b;
+    cout<<"b="<<b<<endl;
+}
