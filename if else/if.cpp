97657b654c1e202596ea62c685708962ef1cef55
vaidahi2023 <vaidahi810@gmail.com>
2026-04-14 15:10:14 +0530

    if else

---
+++ if else/if.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout<<"enter the number:";
+    cin>>a;
+    if(a>10)
+    {
+        cout<<"a is greater than 10";
+    }
+}
---
+++ if else/ifelse.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"enter the number:";
+    cin>>a;
+    cout<<"enter the number:";
+    cin>>b;
+    if(a>b)
+    {
+        cout<<a<<" is maximum number";
+    }
+    else 
+    {
+        cout<<b<<" is maximum number";
+    }
+}
